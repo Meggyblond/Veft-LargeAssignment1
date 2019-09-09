9eb14ac73ce2e51d642772d11bf1400dc264656e
Megg <arnimegg@gmail.com>
2019-09-09 22:03:54 +0000

    Implementing functionality for post requests

--- TechnicalRadiation.Models/Dtos/AuthorDetailDto.cs
+++ TechnicalRadiation.Models/Dtos/AuthorDetailDto.cs
@@ -3,6 +3,7 @@ namespace TechnicalRadiation.Models.Dtos
     public class AuthorDetailDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string ProfielImgSource { get; set; }
         public string Bio { get; set; }
